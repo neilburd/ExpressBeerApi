f589b6a27271128edb0e9cef723c4446638352a8
Danny Kirschner <rkirschner1377@gmail.com>
2017-01-12 23:22:57 -0500

    Adding beer model and create route

---
+++ app/models/beer.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var BeerSchema = new Schema({
+  name: String,
+  kind: String,
+  price: Number
+});
+
+module.exports = mongoose.model('Beer', BeerSchema);
